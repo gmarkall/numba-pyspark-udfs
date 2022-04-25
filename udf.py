c54c4ec3b2117f1eac0fff4f459d04f68dc6cadf
Graham Markall <gmarkall@nvidia.com>
2022-04-25 10:58:49 +0100

    Add Numba-using variant

--- udf.py
+++ udf.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from numba import njit
 
 from datetime import datetime, date
 from pyspark.sql import SparkSession
@@ -12,6 +13,16 @@ def pandas_plus_one(series: pd.Series) -> pd.Series:
     return series + 1
 
 
+@njit
+def numba_plus_one(series):
+    return series + 1
+
+
+@pandas_udf('long')
+def numba_udf_plus_one(series: pd.Series) -> pd.Series:
+    return pd.Series(numba_plus_one(series.to_numpy()))
+
+
 rdd = spark.sparkContext.parallelize([
     (1, 2., 'string1', date(2000, 1, 1), datetime(2000, 1, 1, 12, 0)),
     (2, 3., 'string2', date(2000, 2, 1), datetime(2000, 1, 2, 12, 0)),
@@ -20,3 +31,5 @@ rdd = spark.sparkContext.parallelize([
 df = spark.createDataFrame(rdd, schema=['a', 'b', 'c', 'd', 'e'])
 
 df.select(pandas_plus_one(df.a)).show()
+
+df.select(numba_udf_plus_one(df.a)).show()
